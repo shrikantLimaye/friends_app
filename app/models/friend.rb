b5c97d89875cd068361ad24337f2c2ab9f2476e9
Shrikant Limaye <shrikant.limaye@ritikaawood.com>
2022-09-07 16:56:40 +0530

    Added a defined table

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -2,5 +2,10 @@ class Friend < ApplicationRecord
 
     belongs_to :user
 
+    def full_name
+        result = ''
+        result = "#{first_name}" + ' ' + "#{last_name}"
+        return result
+    end
 
 end
